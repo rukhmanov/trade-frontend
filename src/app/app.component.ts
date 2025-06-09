489f0bf7f7bdf15d4a22de918ba9be73c09a4f1e
rukhman <aleksei.rukhmanov@gmail.com>
2025-06-09 20:48:08 +0300

    xc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 import { App, URLOpenListenerEvent } from '@capacitor/app';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonContent,
+  IonRefresher,
+  IonRefresherContent,
+} from '@ionic/angular/standalone';
 import { register } from 'swiper/element/bundle';
 register();
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [
+    IonRefresherContent,
+    IonRefresher,
+    IonContent,
+    IonApp,
+    IonRouterOutlet,
+  ],
 })
 export class AppComponent {
   constructor(private router: Router, private zone: NgZone) {
@@ -30,5 +42,12 @@ export class AppComponent {
         }
       });
     });
+  }
+
+  handleRefresh(event: CustomEvent) {
+    setTimeout(() => {
+      // Any calls to load data go here
+      (event.target as HTMLIonRefresherElement).complete();
+    }, 2000);
   }
 }
