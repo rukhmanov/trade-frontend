a066e8d92e4d8a761776713044f9df1ad9caf7bc
rukhman <aleksei.rukhmanov@gmail.com>
2025-07-10 16:20:18 +0300

    л

--- src/app/entities/auth/remote-login-back/remote-login-back.component.ts
+++ src/app/entities/auth/remote-login-back/remote-login-back.component.ts
@@ -4,13 +4,18 @@ import { jwtDecode } from 'jwt-decode';
 import { UserStateService } from 'src/app/state/user-state.service';
 import { AuthService } from '../auth.service';
 import { IUser, IYandexResponse, Platform } from '../types';
+import { IonHeader, IonToolbar, IonContent } from '@ionic/angular/standalone';
+import { BackButtonComponent } from '../../back-button/back-button.component';
 
 @Component({
   selector: 'app-remote-login-back',
   templateUrl: './remote-login-back.component.html',
   styleUrls: ['./remote-login-back.component.scss'],
+  imports: [IonContent, IonToolbar, IonHeader, BackButtonComponent],
 })
 export class RemoteLoginBackComponent {
+  access_token: string | null = null;
+
   constructor(
     private route: ActivatedRoute,
     public authService: AuthService,
@@ -25,21 +30,24 @@ export class RemoteLoginBackComponent {
         return acc;
       }, {});
 
+    this.access_token = fragment.access_token;
     this.authService
       .getAndSaveUserData(fragment.access_token)
       .subscribe((data: IUser | any) => {
         const platform = localStorage.getItem('platform');
         userState.token$.next(data.data);
         userState.me$.next(jwtDecode(data.data));
+        localStorage.removeItem('platform');
+        localStorage.removeItem('service');
         switch (platform) {
+          case Platform.web:
+            this.router.navigate(['tabs', 'all']);
+            break;
           case Platform.ios:
             break;
           case Platform.android:
             break;
         }
-        localStorage.removeItem('platform');
-        localStorage.removeItem('service');
-        this.router.navigate(['tabs', 'all']);
       });
   }
 }
